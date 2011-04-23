b8fa95913f95e43215f84592167f6ddf84161b1f
Lucas Rocha <lucasr@lucasr.org>
2011-04-23 01:26:18 +0100

    [wordpress] Move BlogInfoTask inside WidgetConfigure
    It's only used inside WidgetConfigure anyway. Simplifies the code a
    bit as it now has direct access to WidgetConfigure's members. Besides,
    it's a more idiomatic Java style.

--- src/org/lucasr/wordpress/WidgetConfigure.java
+++ src/org/lucasr/wordpress/WidgetConfigure.java
@@ -1,19 +1,28 @@
 package org.lucasr.wordpress;
 
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class WidgetConfigure extends Activity {
 	public static final String PREFS_NAME = "org.lucasr.wordpress.WidgetProvider";
 	public static final String PREFS_KEY_APP_KEY = "app_key_";
 	public static final String PREFS_KEY_BLOG_HOST = "blog_host_";
+
+	private final Context context;
 
 	private int appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
 
@@ -24,17 +33,12 @@ public class WidgetConfigure extends Activity {
 
 	public WidgetConfigure() {
         super();
+
+        context = WidgetConfigure.this;
     }
 
 	private void startLogin() {
-    	final Context context = WidgetConfigure.this;
-
-    	// Hide any error messages here
-
-    	String username = usernameEntry.getText().toString();
-    	String password = passwordEntry.getText().toString();
-
-    	new BlogInfoTask(context, this, appWidgetId, username, password).execute();
+	    new BlogInfoTask().execute();
 	}
 
 	private void updateLoginButtonFromEntries() {
@@ -101,5 +105,111 @@ public class WidgetConfigure extends Activity {
 
         usernameEntry.addTextChangedListener(entryWatcher);
         passwordEntry.addTextChangedListener(entryWatcher);
+    }
+
+    final class BlogInfoTask extends AsyncTask<Void, Void, HashMap<String, String>> {
+        private final String username;
+        private final String password;
+
+        private final View errorLayout;
+        private final TextView errorText;
+        private int errorMessage;
+
+        private final View progressLayout;
+        private final View cancelButton;
+        private final View loginButton;
+
+        public BlogInfoTask() {
+            super();
+
+            username = usernameEntry.getText().toString();
+            password = passwordEntry.getText().toString();
+
+            progressLayout = findViewById(R.id.progress_layout);
+            errorLayout = findViewById(R.id.error_layout);
+
+            errorText = (TextView) findViewById(R.id.error_text);
+
+            cancelButton = findViewById(R.id.cancel_button);
+            loginButton = findViewById(R.id.login_button);
+        }
+
+        private void saveBlogInfo(String apiKey, String blogId) {
+            SharedPreferences.Editor prefs =
+                context.getSharedPreferences(WidgetConfigure.PREFS_NAME, 0).edit();
+
+            prefs.putString(WidgetConfigure.PREFS_KEY_APP_KEY + appWidgetId, apiKey);
+            prefs.putString(WidgetConfigure.PREFS_KEY_BLOG_HOST + appWidgetId, blogId);
+
+            prefs.commit();
+        }
+
+        private void updateAppWidget(String apiKey, String blogId) {
+            // Fetch latest stats and update the widget accordingly
+            new StatsTask(context, appWidgetId, apiKey, blogId).execute();
+        }
+
+        private void updateResultAndFinish() {
+            Intent resultValue = new Intent();
+            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+
+            setResult(Activity.RESULT_OK, resultValue);
+            finish();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            Log.v("WordpressBlogInfoTask", "onPreExecute()");
+
+            cancelButton.setEnabled(false);
+            loginButton.setEnabled(false);
+
+            progressLayout.setVisibility(View.VISIBLE);
+            errorLayout.setVisibility(View.GONE);
+        }
+
+        @Override
+        protected HashMap<String, String> doInBackground(Void... arg0) {
+            Log.v("WordpressBlogInfoTask", "doInBackground()");
+
+            BlogInfo blogInfo = new BlogInfo(username, password);
+
+            try {
+                return blogInfo.getInfo();
+            } catch (NoAuthException nae) {
+                errorMessage = R.string.login_error_auth;
+                return null;
+            } catch (NetworkException ne) {
+                errorMessage = R.string.login_error_network;
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(HashMap<String, String> blogInfo) {
+            Log.v("WordpressBlogInfoTask", "onPostExecute()");
+
+            String apiKey = null;
+            String blogId = null;
+
+            if (blogInfo != null) {
+                apiKey = blogInfo.get("apiKey");
+                blogId = blogInfo.get("blogId");
+            }
+
+            if (apiKey == null || blogId == null) {
+                progressLayout.setVisibility(View.GONE);
+                errorLayout.setVisibility(View.VISIBLE);
+
+                errorText.setText(errorMessage);
+
+                cancelButton.setEnabled(true);
+                loginButton.setEnabled(true);
+            } else {
+                saveBlogInfo(apiKey, blogId);
+                updateAppWidget(apiKey, blogId);
+                updateResultAndFinish();
+            }
+        }
     }
 }
